6f0f455c19a21fde3669297ce5fadc7e752f29aa
otherPOM <otherPOM@gmail.com>
2020-12-19 16:59:24 +0300

    Stage 1 done

--- SimpleBot.java
+++ SimpleBot.java
@@ -1,7 +1,10 @@
 package bot;
 
 public class SimpleBot {
+    private static String botName = "Charlie";
+    private static int botBirthYear = 2020;
     public static void main(String[] args) {
-        // write your code here
+        System.out.printf("Hello! My name is %s.\n" +
+                "I was created in %d.\n", botName, botBirthYear);
     }
 }
